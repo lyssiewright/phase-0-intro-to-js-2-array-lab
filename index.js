e88aa46e6351528be02b606acc8c9d0ffdf4f11d
Lyssie Wright <lyssiewright.15@gmail.com>
2022-01-16 18:01:08 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(2);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(0);
+    return cats;
+}
+
+function appendCat(name){
+    const allCats = [...cats, "Broom"];
+    return allCats;
+}
+
+function prependCat(name){
+    const addCat = ["Arnold", ...cats];
+    return addCat;
+}
+
+function removeLastCat(name){
+    const minusCatFromEnd = cats.slice(0, cats.length - 1);
+    return minusCatFromEnd;
+}
+
+function removeFirstCat(name){
+    const minusCatFromBeginning = cats.slice(1);
+    return minusCatFromBeginning;
+}
